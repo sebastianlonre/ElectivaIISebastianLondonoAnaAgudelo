17da0ce3edc9b8fd29885f1e63e823f7c605924f
ANAISA123 <ana.agudelo95@correo.tdea.edu.co>
2024-08-10 17:04:54 -0500

    ADD Controller register with respective validations, and ADD routes .get and .post in the file apiRoutes.

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,42 @@ const login = (request, response) => {
   });
 };
 
+const register = (request, response) => {
+  const {  uid, email, userName, password  } = request.body;
+
+  if (!uid || !email || !userName || !password) {
+    return response.status(400).json({
+      error: "Todos los campos son obligatorios, por favor completelos",
+    });
+  }
+  const emailExist = users.some((user) => user.email === email);
+  const uidExist = users.some((user) => user.uid === uid);
+
+  if (emailExist || uidExist) {
+    return response.status(409).json({
+      error: "Ya existe un usuario con el uid o email ingresados",
+    });
+  }
+  const newUser = {
+       uid,
+       email,
+       userName,
+       password,
+     
+  };
+  users.push(newUser);
+
+  const userList = users.map(({ password, ...user }) => user);
+
+  return response.status(201).json({
+    message: "Usuario registrado correctamente.",
+    users: userList,
+  });
+};
+
+
 
 module.exports = {
   login,
+  register,
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const { listTweets, newTweets } = require("../controllers/tweetsController");
 const router = express.Router();
+const { login, register } = require("../controllers/userController");
 
 router.get("/tweets", listTweets);
+
+//login
+router.get("/user", login);
+
+//register
+router.post("/user/register",register );
 
 module.exports = router;

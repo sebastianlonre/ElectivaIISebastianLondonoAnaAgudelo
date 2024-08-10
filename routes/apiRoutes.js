95347357aefd84070eac27a9b37e4e7f0c751342
sebastianlonre <sebastian.londono75@correo.tdea.edu.co>
2024-08-10 16:21:54 -0500

    ADD listFollows and follow user

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { listTweets, newTweets, myTweets, deleteTweet } = require("../controllers/tweetsController");
+const { listMysocial, followUser } = require("../controllers/socialController");
 const router = express.Router();
 
 
@@ -8,5 +9,10 @@ router.get("/tweets", listTweets);
 router.post("/tweets", newTweets);
 router.get("/myTweets", myTweets);
 router.delete("/tweets/:idTweets", deleteTweet);
+
+//Social
+
+router.get("/social/:uid", listMysocial);
+router.post("/social", followUser)
 
 module.exports = router;

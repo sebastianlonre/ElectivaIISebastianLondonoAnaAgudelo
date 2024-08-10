54a845ea381b9d923ef55e4a95ee99b6d864bff4
sebastianlonre <sebastian.londono75@correo.tdea.edu.co>
2024-08-10 09:12:13 -0500

    ADD get tweets and routers

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const router = require("./routes/apiRoutes");
+
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`La app esta escuchando en <http://localhost>:${port}`);
